199ccb6fa29882fb043663b52cccee59925882f2
Nalin Adhikari <nalin@fusemachines.com>
2018-02-14 16:32:03 +0545

    spring boot admin client configured and runnable

---
+++ src/main/java/com/server/client/SpringBootAdminClientApplication.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.server.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author nalin
+ *
+ */
+@SpringBootApplication
+public class SpringBootAdminClientApplication {
+
+	public static void main(String[] args) {
+        SpringApplication.run(SpringBootAdminClientApplication.class, args);
+    }
+}
